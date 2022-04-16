c98c9a9dec16686a5bc5aecaa313bde45dbd34b8
chentitorupa <ruizvicente020@gmail.com>
2022-04-16 15:17:54 -0500

    Limpieza de registros en la tabla de status

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220416015436) do
+ActiveRecord::Schema.define(version: 20220416201100) do
 
   create_table "statuses", force: :cascade do |t|
-    t.string "name"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_statuses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
