d3c7a9cc3ee8fb6db68cf63710bf2e9a1cba374e
chentitorupa <ruizvicente020@gmail.com>
2022-04-20 15:24:28 -0500

    Se crearon rutas de acceso personalizadas con devise_scope

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+    def flash_class(type)
+        case type
+        when 'alert'
+            then "alert-danger"
+        when 'notice'
+            then "alert-success"
+        else
+            ""
+        end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  devise_scope :user do
+    get 'registrar', to: 'devise/registrations#new', as: :registrar
+    get 'iniciarSesion', to: 'devise/sessions#new', as: :iniciarSesion
+    get 'cerrarSesion', to: 'devise/sessions#destroy', as: :cerrarSesion
+  end
+
   resources :statuses
+  get 'timeline', to: 'statuses#index', as: :timeline
   root to: "statuses#index"
+
+  get '/:id', to: 'perfil#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
