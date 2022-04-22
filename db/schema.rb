c9532651ba7462c2a4674aac37f77197250d9ecb
chentitorupa <ruizvicente020@gmail.com>
2022-04-21 21:28:06 -0500

    Se cambió a base de datos postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20220416201100) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "statuses", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
